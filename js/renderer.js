6b8577192d0c52db4a6ff1e43764c012b3852d4d
THESHMIL0 <loolk448@gmail.com>
2026-03-12 20:11:07 +0530

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -8,27 +8,39 @@ class Renderer {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.drawBoard();
+        this.drawParticles();
         this.drawCoins();
     }
 
     drawBoard() {
-        // Wooden board background
-        this.ctx.fillStyle = '#d4a373';
+        // Wood finish
+        this.ctx.fillStyle = '#e5aa70';
         this.ctx.fillRect(0, 0, this.game.boardSize, this.game.boardSize);
         
         // Borders
-        this.ctx.fillStyle = '#5c4033';
+        this.ctx.fillStyle = '#4a2e1b';
         this.ctx.fillRect(0, 0, this.game.boardSize, this.game.margin);
         this.ctx.fillRect(0, this.game.boardSize - this.game.margin, this.game.boardSize, this.game.margin);
         this.ctx.fillRect(0, 0, this.game.margin, this.game.boardSize);
         this.ctx.fillRect(this.game.boardSize - this.game.margin, 0, this.game.margin, this.game.boardSize);
 
-        // Center circles
-        this.ctx.beginPath();
-        this.ctx.arc(400, 400, 60, 0, Math.PI * 2);
-        this.ctx.strokeStyle = '#c0392b';
-        this.ctx.lineWidth = 3;
-        this.ctx.stroke();
+        // Center art
+        this.ctx.beginPath(); this.ctx.arc(400, 400, 60, 0, Math.PI * 2);
+        this.ctx.strokeStyle = '#c0392b'; this.ctx.lineWidth = 3; this.ctx.stroke();
+        this.ctx.beginPath(); this.ctx.arc(400, 400, 10, 0, Math.PI * 2);
+        this.ctx.fillStyle = '#c0392b'; this.ctx.fill();
+
+        // Baselines (Where you place the striker)
+        this.ctx.strokeStyle = '#333'; this.ctx.lineWidth = 2;
+        this.ctx.beginPath(); this.ctx.moveTo(200, 150); this.ctx.lineTo(600, 150); this.ctx.stroke(); // AI Baseline
+        this.ctx.beginPath(); this.ctx.moveTo(200, 650); this.ctx.lineTo(600, 650); this.ctx.stroke(); // Player Baseline
+        
+        // Baseline circles
+        this.ctx.fillStyle = '#c0392b';
+        [ [200,150], [600,150], [200,650], [600,650] ].forEach(pos => {
+            this.ctx.beginPath(); this.ctx.arc(pos[0], pos[1], 15, 0, Math.PI*2); this.ctx.fill();
+            this.ctx.stroke();
+        });
 
         // Pockets
         this.ctx.fillStyle = '#111';
@@ -43,27 +55,53 @@ class Renderer {
         for (let coin of this.game.coins) {
             if (!coin.active) continue;
             
+            // 3D Gradient effect
+            let grad = this.ctx.createRadialGradient(coin.pos.x - 5, coin.pos.y - 5, coin.radius * 0.2, coin.pos.x, coin.pos.y, coin.radius);
+            grad.addColorStop(0, '#fff'); // Highlight
+            grad.addColorStop(0.3, coin.color); 
+            grad.addColorStop(1, '#000'); // Shadow edge
+
             this.ctx.beginPath();
             this.ctx.arc(coin.pos.x, coin.pos.y, coin.radius, 0, Math.PI * 2);
-            this.ctx.fillStyle = coin.color;
+            this.ctx.fillStyle = grad;
             this.ctx.fill();
             
-            // Add a little sheen to make them look 3D
+            // Striker ring pattern
+            if (coin.type === 'striker') {
+                this.ctx.beginPath();
+                this.ctx.arc(coin.pos.x, coin.pos.y, coin.radius * 0.6, 0, Math.PI * 2);
+                this.ctx.strokeStyle = '#3498db';
+                this.ctx.lineWidth = 2;
+                this.ctx.stroke();
+            }
+        }
+    }
+
+    drawParticles() {
+        for (let p of this.game.particles) {
+            this.ctx.globalAlpha = p.life;
+            this.ctx.fillStyle = p.color;
             this.ctx.beginPath();
-            this.ctx.arc(coin.pos.x - 3, coin.pos.y - 3, coin.radius * 0.4, 0, Math.PI * 2);
-            this.ctx.fillStyle = 'rgba(255,255,255,0.2)';
+            this.ctx.arc(p.pos.x, p.pos.y, 3, 0, Math.PI * 2);
             this.ctx.fill();
         }
+        this.ctx.globalAlpha = 1.0;
     }
 
     drawAimLine(startPoint, endPoint) {
         this.ctx.beginPath();
         this.ctx.moveTo(startPoint.x, startPoint.y);
         this.ctx.lineTo(endPoint.x, endPoint.y);
-        this.ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)';
-        this.ctx.lineWidth = 2;
-        this.ctx.setLineDash([5, 5]);
+        this.ctx.strokeStyle = 'rgba(255, 255, 255, 0.6)';
+        this.ctx.lineWidth = 3;
+        this.ctx.setLineDash([8, 8]);
         this.ctx.stroke();
         this.ctx.setLineDash([]);
+        
+        // Draw target circle at the end of the line
+        this.ctx.beginPath();
+        this.ctx.arc(endPoint.x, endPoint.y, this.game.striker.radius, 0, Math.PI*2);
+        this.ctx.strokeStyle = 'rgba(255,255,255,0.3)';
+        this.ctx.stroke();
     }
 }
